5fdb5a8ada0518527f43264ac16cdfe7d0c528dd
danilocarrasco <danilo.eletronica@gmail.com>
2019-03-05 00:53:59 -0300

    SplashScreen + Tela Inicial com Login e cadastro + Tela cadastro ainda sem up de dados + tela QR Code

--- app/src/main/java/com/example/autentic/Activity/Qrcode.java
+++ app/src/main/java/com/example/autentic/Activity/Qrcode.java
@@ -1,12 +1,25 @@
 package com.example.autentic.Activity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.autentic.R;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import static com.google.zxing.integration.android.IntentIntegrator.parseActivityResult;
 
 public class Qrcode extends AppCompatActivity {
+
+    Button btnScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +28,37 @@ public class Qrcode extends AppCompatActivity {
 
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        btnScan = (Button) findViewById(R.id.btnqrcode);
+        final Activity activity = this;
+
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setCameraId(0);
+                integrator.initiateScan();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if (result != null) {
+            if (result.getContents() != null) {
+                alert(result.getContents());
+            } else {
+                alert("Leitura cancelada");
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void alert(String msg){
+        Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_LONG).show();
     }
 }
